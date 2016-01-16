efbd84203f0b2db126a0db878046bdfc3bf6708e
Peter Thaleikis <petert@catalyst.net.nz>
2016-01-17 11:58:23 +1300

    MINOR: adding phpdoc blocks

--- code/models/NewsTag.php
+++ code/models/NewsTag.php
@@ -2,21 +2,36 @@
 
 class NewsTag extends DataObject
 {
+    /**
+     * @var array
+     */
     private static $db = array(
         "Title" => "Varchar"
     );
 
+    /**
+     * @var array
+     */
     private static $belongs_many_many = array(
         "NewsArticles" => "NewsArticle"
     );
 
+    /**
+     * @var array
+     */
     private static $summary_fields = array(
         'Title' => 'Title',
         'NewsArticles.Count' => 'Number of articles'
     );
 
+    /**
+     * @var string
+     */
     private static $default_sort = 'Title ASC';
 
+    /**
+     * @return FieldList
+     */
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
